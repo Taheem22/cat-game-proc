4742a65d334961819ff3eaeb609cc1ab70b9a827
Taheem22 <taheem.hasan@laetottenham.ac.uk>
2024-10-31 14:11:16 +0000

    exclamation moark

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ name = input("Enter name:")
 # start the while loop
 while True:
     # Finish the string below
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
+    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats!")
 
     if option == '1':
         # change the cat's attributes here
